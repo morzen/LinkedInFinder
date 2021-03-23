3338cef097ae798e9fb9c6d097809db00a747795
morzen <barn.mp@icloud.com>
2021-03-23 14:47:47 +0000

    new modifications plus logs added

--- LinkedInFinder.py
+++ LinkedInFinder.py
@@ -8,6 +8,10 @@ from bs4 import BeautifulSoup
 import sys
 import re
 import json
+import logging
+
+#comment/uncomment the line underneath to have debug log displayed/not displayed
+#logging.basicConfig(level=logging.DEBUG)
 
 i = 0
 lookuplist = []
@@ -17,23 +21,29 @@ root= tk.Tk()
 data = {}
 FinalDictionnary = {}
 
-user = "MrPotatoTheTastiest@protonmail.com"
-Passwd = "FrenchFries8"
+#please enter your credential email and password
+user = ""
+Passwd = ""
 
-
+#open the List of url to go through it and look them up
 with open('ListURL.txt') as f:
     lines = f.read().splitlines()
     lookuplist = lines
 
 listlength = len(lookuplist)
+
+# you will be told the length of your list at the very beginning
 print(listlength)
 
+#important verify that the geckodriver is present in the folder
+# and check for the lastest version if this one doesn't work
 browser = webdriver.Firefox(executable_path="./geckodriver")
 
 yolo = lookuplist[i]    
 
+#first page it will look for is obviously the login
 browser.get('https://www.linkedin.com/uas/login')
-
+#it will enter your credential and log as you
 ID = browser.find_element_by_id('username')
 ID.send_keys(user)
 
@@ -46,6 +56,11 @@ time.sleep(10)
 
 browser.execute_script("window.open('');")
 
+#here is the big while loop that will go through all URL in the list 
+#load the page and take the information needed
+# if modification is necessary i hope you know HTML better than I
+# to select the information you have to manually go on the desired page and look
+# at the source code and selecte the specific place where the information is stored
 while i < listlength:
     file_object = open('TempResult.txt','a')
     info = []
@@ -72,7 +87,8 @@ while i < listlength:
     
     info.append(yolo)
 
-    
+    #those try and except are here to avoid errors due to differences
+    #in the source code of different pages
     try:
         PageSRC = browser.page_source
         FilteredSRC = BeautifulSoup(PageSRC, "lxml")
@@ -133,7 +149,9 @@ while i < listlength:
     except:
         #print('went the other way')
         companyname = aTags.find('h3').find_all('span')[1].get_text().strip()
-        #print('company name: ', companyname)
+        
+        logging.debug('company name: %s \n', companyname)
+        
         info.append("ERROR")
 
 
@@ -152,19 +170,24 @@ while i < listlength:
             info.append(duration)
         except:
             try:
-       #print('went the other way')
+                #print('went the other way')
                 duration = aTags.find_all('h4')
-       #print(duration)
+                #print(duration)
                 durationSTR = str(duration)
-        
-       #print('\n', durationSTR, '\n')
+                logging.debug("\n")
+                logging.debug(durationSTR)
+                logging.debug("\n")
+                
                 newSTR = durationSTR.split("\n",2)[2]
        
-       #print('newstring: ', newSTR)
+                logging.debug('newstring: %s \n', newSTR)
+                
                 start = newSTR.find("<span>") + len("<span>")
                 end = newSTR.find("</span>")
                 substring = newSTR[start:end]
-       #print('duration: ',substring)
+                
+                logging.debug('duration: %s \n',substring)
+
                 duration = substring
                 info.append(duration)
             except:
@@ -174,10 +197,11 @@ while i < listlength:
 
     #print(info)
     
+    #here is whereeverything is store
     name = info[1]
     name2 = info[1]
     name2 = name2.replace(' ', '.')
-    email = name2 + '@hs2.org.uk'
+    email = name2 + '@EnterHostnameOfTheCompany.com'
     
     info2.append(email)
     info2.append(yolo)
--- README.md
+++ README.md
@@ -10,4 +10,18 @@ Welcome and thank you for choosing LInkedinFinder to stalk your mates.
 We hope you will have a pleasant journey on the interweb with us.
 
 This script uses:
+Selenium (might have to pip3 install that)
+
+You will also need to enter your credential for linkedin User id and password 
+(I suggest creating a fake account)
+
+Once all that is done put your list of URL linkedin profile in ListURL.txt
+the result will appear in a file called TempResult.txt
+
+WORD OF WARNING
+
+if the script fail at some point (probably due to and html page being slightly different than others)
+do not panick all your results are stored in the TempResult.txt and the script is meant append anything to this file and not delete or overwrite
+but i would recomment you look at what number the script faild and to modify the i=0 line 12 by the number at where it failed and  if it fail again to the next number.
+like that you would avoid appenning result you already have.
 
